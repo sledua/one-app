f24ce063db64c2780be34c36f61e5ed162e323ae
InnaGrim <innagrim95@gmail.com>
2024-06-06 23:57:02 +0300

    Fix
    Арифметика і написала адресу у строку

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-console.log("Hello World 123")
-let pp = document.getElementById('o');
-pp.innerText = "inner Text";
-console.log(pp)
+// Базові арифметичні оператори
+
+const width = 5*3;
+const height = 3+3;
+const space = width*height;
+const newWidth = width - 3;
+const volume = 3**3;
+console.log(volume)
+
+// Строки
+const city = 'Київ';
+const street = 'Дарницький бульвар';
+const appart = 'кв.15';
+console.log (city + ', ' + street + ' ' + 12 + ', ' + appart);
